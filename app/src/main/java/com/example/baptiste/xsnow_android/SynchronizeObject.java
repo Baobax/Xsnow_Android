65fdca0fe8079c86b80a6b0aa83616af179ced3b
Baptiste <baptiste.henot@free.fr>
2017-10-25 16:21:06 +0200

    Ajout d'un objet qui sert à synchroniser le onSizeChanged et le draw pour régler le bug dans le multi fenêtres

---
+++ app/src/main/java/com/example/baptiste/xsnow_android/SynchronizeObject.java
@@ -0,0 +1,12 @@
+package com.example.baptiste.xsnow_android;
+
+/**
+ * Created by baptiste on 25/10/2017.
+ */
+
+public class SynchronizeObject {
+    public void pauseOtherThread() {
+        // The only purpose of this method and this object is to block other threads with the use of "synchronized()"
+        // I just created this method to have an explicit name to call
+    }
+}
