41454aae5143ba5d6bd08e55443447b40c814c27
Baptiste <baptiste.henot@free.fr>
2017-10-30 11:15:32 +0100

    Augmentation de : taille cases + taille images + temps dans le sleep

--- app/src/main/java/com/example/baptiste/xsnow_android/CustomView.java
+++ app/src/main/java/com/example/baptiste/xsnow_android/CustomView.java
@@ -51,10 +51,10 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
         Log.i(TAG, "Width : " + mWidth + ", Height : " + mHeight);
         Log.i(TAG, "CWidth : " + canvas.getWidth() + ", CHeight : " + canvas.getHeight());
         canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        for (int y = (mHeight / 10) - 1; y >= 0; y--) {
-            for (int x = (mWidth / 10) - 1; x >= 0; x--) {
+        for (int y = (mHeight / 15) - 1; y >= 0; y--) {
+            for (int x = (mWidth / 15) - 1; x >= 0; x--) {
                 if ((mZoneDessin[x][y].isImage())) {
-                    canvas.drawBitmap(mZoneDessin[x][y].getImage(), x * 10, y * 10, null);
+                    canvas.drawBitmap(mZoneDessin[x][y].getImage(), x * 15, y * 15, null);
                 }
             }
         }
@@ -75,17 +75,17 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
 
     public void createElement(boolean vent) {
         if (vent) {
-            int y = mRand.nextInt(mHeight / 10);
+            int y = mRand.nextInt(mHeight / 15);
             if (mRand.nextInt(2) == 0) {
-                mZoneDessin[(mWidth / 10) - 1][y].setImage(mImage1);
+                mZoneDessin[(mWidth / 15) - 1][y].setImage(mImage1);
             } else {
-                mZoneDessin[(mWidth / 10) - 1][y].setImage(mImage2);
+                mZoneDessin[(mWidth / 15) - 1][y].setImage(mImage2);
             }
             int maxVitesse = mRand.nextInt(31 - 20) + 20;
             int direction = mRand.nextInt(maxVitesse);
-            mZoneDessin[(mWidth / 10) - 1][y].setDirectionVitesse(direction, maxVitesse);
+            mZoneDessin[(mWidth / 15) - 1][y].setDirectionVitesse(direction, maxVitesse);
         } else {
-            int x = mRand.nextInt(mWidth / 10);
+            int x = mRand.nextInt(mWidth / 15);
             if (mRand.nextInt(2) == 0) {
                 mZoneDessin[x][0].setImage(mImage1);
             } else {
@@ -105,10 +105,10 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
             super.onSizeChanged(w, h, oldw, oldh);
             mWidth = w;
             mHeight = h;
-            mZoneDessin = new ZoneDessin[mWidth / 10][mHeight / 10];
-            for (int y = (mHeight / 10) - 1; y >= 0; y--) {
-                for (int x = (mWidth / 10) - 1; x >= 0; x--) {
-                    int n = mRand.nextInt(10000);
+            mZoneDessin = new ZoneDessin[mWidth / 15][mHeight / 15];
+            for (int y = (mHeight / 15) - 1; y >= 0; y--) {
+                for (int x = (mWidth / 15) - 1; x >= 0; x--) {
+                    int n = mRand.nextInt(15000);
                     if (n == 0) {
                         if (mRand.nextInt(2) == 0) {
                             mZoneDessin[x][y] = new ZoneDessin(mImage1);
@@ -173,10 +173,10 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
                 Canvas canvas = null;
                 synchronized (mSynchronizedObject) {
                     mSynchronizedObject.pauseOtherThread();
-                    if (mCount % 2000 >= 1900 && mCount % 2000 <= 1999) { // wind to reinitialize the drawing zone
+                    if (mCount % 4000 >= 3900 && mCount % 4000 <= 3999) { // wind to reinitialize the drawing zone
                         mVent = true;
-                        for (int x = 0; x < mWidth / 10; x++) {
-                            for (int y = (mHeight / 10) - 1; y >= 0; y--) {
+                        for (int x = 0; x < mWidth / 15; x++) {
+                            for (int y = (mHeight / 15) - 1; y >= 0; y--) {
                                 if ((mZoneDessin[x][y].isImage())) {
                                     if (x - 4 >= 0) {
                                         if (mCount % 2000 > 1980) {
@@ -197,8 +197,8 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
                         }
                     } else { // normal behavior without wind
                         mVent = false;
-                        for (int y = (mHeight / 10) - 2; y >= 0; y--) {
-                            for (int x = (mWidth / 10) - 1; x >= 0; x--) {
+                        for (int y = (mHeight / 15) - 2; y >= 0; y--) {
+                            for (int x = (mWidth / 15) - 1; x >= 0; x--) {
                                 if ((mZoneDessin[x][y].isImage())) {
                                     particleDirection = mZoneDessin[x][y].getUpdatedDirection();
                                     calculMouvementParticule(particleDirection, x, y);
@@ -234,14 +234,14 @@ public class CustomView extends SurfaceView implements SurfaceHolder.Callback {
                 }
 
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(15);
                 } catch (InterruptedException e) {
                 }
             }
         }
 
         private void calculMouvementParticule(int particleDirection, int x, int y) {
-            if (x + particleDirection < mWidth / 10 && x + particleDirection >= 0) {
+            if (x + particleDirection < mWidth / 15 && x + particleDirection >= 0) {
                 if (mZoneDessin[x + particleDirection][y + 1].isImage()) {
                     if (!mZoneDessin[x][y + 1].isImage()) {
                         mZoneDessin[x][y + 1].deplacerElement(mZoneDessin[x][y].getImage(), mZoneDessin[x][y].getDirection(), mZoneDessin[x][y].getMaxVitesseDirection());
